6ceab0a977ce053df31be0f5c55e076b0dd6b876
yakov <yakovrussian@gmail.com>
2023-02-19 22:01:39 +0300

    Stopped calling launch inside launch. Now using Collection<Job>.joinAll() to wait for the longest launched coroutine, not all one by one.

--- app/src/main/java/com/example/sportapp/model/Repo.kt
+++ app/src/main/java/com/example/sportapp/model/Repo.kt
@@ -7,67 +7,56 @@ import com.example.sportapp.model.data_classes.fixtures.Fixtures
 import com.example.sportapp.model.retrofit.SportApi
 import com.example.sportapp.model.room.MatchesDatabase
 import com.example.sportapp.utils.Resource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import retrofit2.Response
 
-class Repo private constructor(){
+class Repo private constructor() {
 
-    suspend fun requestAllFixtures() : Resource<List<Fixtures>> {
+    suspend fun requestAllFixtures(): Resource<List<Fixtures>> {
         var result: Resource<MutableList<Fixtures>> = Resource.Loading()
-        try {
-            CoroutineScope(Dispatchers.IO).launch {
-                val results : MutableList<Response<Fixtures>> = mutableListOf()
-                roundsOfLeagues.forEach {
-                    results.add(
-                        async{ SportApi.getInstance().requestFixtures(roundId = it) }.await()
-                    )
-                }
-                results.forEach {
-                    if (result !is Resource.Success) {
-                        result = Resource.Success(data = mutableListOf(it.body()!!))
-                    } else result.data!!.add(it.body()!!)
-                }
-            }.join()
-        } catch (e : Exception) {
-            Log.d("Repo", e.message ?: ":(")
-            result = Resource.Error("Failed request")
+        withContext(Dispatchers.IO) {
+            val results: MutableList<Response<Fixtures>> = mutableListOf()
+            val jobs = mutableListOf<Job>()
+            roundsOfLeagues.forEach {
+                jobs.add(
+                    launch {
+                        results.add(SportApi.getInstance().requestFixtures(roundId = it))
+                    }
+                )
+            }
+            jobs.joinAll()
+            results.forEach {
+                if (!it.isSuccessful) return@forEach
+                if (result !is Resource.Success) {
+                    result = Resource.Success(data = mutableListOf(it.body()!!))
+                } else result.data!!.add(it.body()!!)
+            }
         }
         return result as Resource<List<Fixtures>>
     }
 
     suspend fun saveMatch(match: Data, context: Context) {
-        CoroutineScope(Dispatchers.IO).launch {
-            MatchesDatabase(context).getMatchesDao().saveMatch(match)
-        }
+        MatchesDatabase(context).getMatchesDao().saveMatch(match)
     }
 
-    suspend fun getSavedMatches(context: Context) : List<Data> {
-        var matches = listOf<Data>()
-        CoroutineScope(Dispatchers.IO).launch {
-            matches = async{ MatchesDatabase(context).getMatchesDao().getSavedMatches() }.await()
-        }.join()
-        return matches
+    suspend fun getSavedMatches(context: Context): List<Data> {
+        return MatchesDatabase(context).getMatchesDao().getSavedMatches()
     }
 
     suspend fun deleteMatch(match: Data, context: Context) {
-        CoroutineScope(Dispatchers.IO).launch {
-            MatchesDatabase(context).getMatchesDao().deleteMatch(match)
-        }
+        MatchesDatabase(context).getMatchesDao().deleteMatch(match)
     }
 
     companion object {
         var repo: Repo? = null
-        fun getInstance() : Repo {
+        fun getInstance(): Repo {
             if (repo == null) {
                 repo = Repo()
             }
             return repo!!
         }
 
-                              //superliga, aLeague, TipicoBundesliga
+        //superliga, aLeague, TipicoBundesliga
         val roundsOfLeagues = listOf(151633, 157592, 152556)
     }
 }
