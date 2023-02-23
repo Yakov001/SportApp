ffbe730934c55450899973dfb67c5426794faaf9
yakov <yakovrussian@gmail.com>
2023-02-23 15:59:26 +0300

    using extension functions in Mapper

--- app/src/main/java/com/example/sportapp/data/SportRepoImpl.kt
+++ app/src/main/java/com/example/sportapp/data/SportRepoImpl.kt
@@ -2,24 +2,23 @@ package com.example.sportapp.data
 
 import android.app.Application
 import android.util.Log
-import com.example.sportapp.data.network.entities.fixtures.FixturesDto
-import com.example.sportapp.data.network.SportApi
 import com.example.sportapp.data.database.GameDatabase
-import com.example.sportapp.data.mapper.GameMapper
+import com.example.sportapp.data.mapper.toDbModel
+import com.example.sportapp.data.mapper.toEntity
+import com.example.sportapp.data.network.SportApi
+import com.example.sportapp.data.network.entities.fixtures.FixturesDto
 import com.example.sportapp.domain.Resource
 import com.example.sportapp.domain.SportRepository
 import com.example.sportapp.domain.entities.Game
 import kotlinx.coroutines.*
 import retrofit2.Response
 import java.io.IOException
-import java.net.SocketTimeoutException
 
 class SportRepoImpl private constructor(
     private val app: Application
 ) : SportRepository {
 
     private val matchesDao = GameDatabase(app).getGameDao()
-    private val mapper = GameMapper()
 
     override suspend fun requestAllGames(): Resource<List<List<Game>>> {
         var result: Resource<MutableList<List<Game>>> = Resource.Loading()
@@ -48,27 +47,27 @@ class SportRepoImpl private constructor(
                 if (!response.isSuccessful) return@forEach
                 if (result !is Resource.Success) {
                     result = Resource.Success(data = mutableListOf(response.body()!!.data.map {
-                        mapper.mapDtoToEntity(it)
+                        it.toEntity()
                     }))
-                } else result.data!!.add(response.body()!!.data.map { mapper.mapDtoToEntity(it) })
+                } else result.data!!.add(response.body()!!.data.map { it.toEntity() })
             }
         }
         return result as Resource<List<List<Game>>>
     }
 
     override suspend fun getSavedGames(): List<Game> {
-        return matchesDao.getSavedGames().map { mapper.mapDbModelToEntity(it) }
+        return matchesDao.getSavedGames().map { it.toEntity() }
     }
 
     override suspend fun saveGame(game: Game) {
         matchesDao.saveGame(
-            mapper.mapEntityToDbModel(game)
+            game.toDbModel()
         )
     }
 
     override suspend fun deleteGame(game: Game) {
         matchesDao.deleteGame(
-            mapper.mapEntityToDbModel(game)
+            game.toDbModel()
         )
     }
 
--- app/src/main/java/com/example/sportapp/data/mapper/GameMapper.kt
+++ app/src/main/java/com/example/sportapp/data/mapper/GameMapper.kt
@@ -7,125 +7,121 @@ import com.example.sportapp.data.network.entities.fixtures.*
 import com.example.sportapp.data.network.entities.fixtures.League
 import com.example.sportapp.domain.entities.*
 
-class GameMapper {
+fun GameDto.toDbModel(): GameDbModel {
+    return GameDbModel(
+        id = this.id,
+        league = LeagueDbModel(
+            name = this.league.name,
+            country_name = this.league.country_name,
+            country_flag = this.league.country_flag
+        ),
+        venueId = this.venue_id,
+        stageId = this.stage_id,
+        stageName = this.stage_name,
+        roundName = this.round_name,
 
-    fun mapDtoToDbModel(dto: GameDto) : GameDbModel {
-        return GameDbModel(
-            id = dto.id,
-            league = LeagueDbModel(
-                name = dto.league.name,
-                country_name = dto.league.country_name,
-                country_flag = dto.league.country_flag
+        time = this.time.time,
+        dateTime = this.time.datetime,
+        timeStamp = this.time.timestamp,
+
+        homeTeam = TeamDbModel(
+            name = this.teams.home.name,
+            image = this.teams.home.img
+        ),
+        awayTeam = TeamDbModel(
+            name = this.teams.away.name,
+            image = this.teams.away.img
+        )
+    )
+}
+
+fun GameDbModel.toDto(): GameDto {
+    return GameDto(
+        id = this.id,
+        league = League(
+            country_flag = this.league.country_flag,
+            country_name = this.league.country_name,
+            name = this.league.name
+        ),
+        teams = Teams(
+            away = Away(
+                img = this.awayTeam.image,
+                name = this.awayTeam.name
             ),
-            venueId = dto.venue_id,
-            stageId = dto.stage_id,
-            stageName = dto.stage_name,
-            roundName = dto.round_name,
-
-            time = dto.time.time,
-            dateTime = dto.time.datetime,
-            timeStamp = dto.time.timestamp,
-
-            homeTeam = TeamDbModel(
-                name = dto.teams.home.name,
-                image = dto.teams.home.img
-            ),
-            awayTeam = TeamDbModel(
-                name = dto.teams.away.name,
-                image = dto.teams.away.img
+            home = Home(
+                img = this.homeTeam.image,
+                name = this.homeTeam.name
             )
+        ),
+        venue_id = this.venueId,
+        stage_id = this.stageId,
+        stage_name = this.stageName,
+        time = Time(
+            time = this.time,
+            datetime = this.dateTime,
+            timestamp = this.timeStamp
+        ),
+        round_name = this.roundName
+    )
+}
+
+fun GameDto.toEntity(): Game {
+    return Game(
+        id = this.id,
+        league = com.example.sportapp.domain.entities.League(
+            name = this.league.name,
+            country_name = this.league.country_name,
+            country_flag = this.league.country_flag
+        ),
+        venueId = this.venue_id,
+        stageId = this.stage_id,
+        stageName = this.stage_name,
+        roundName = this.round_name,
+
+        time = this.time.time,
+        dateTime = this.time.datetime,
+        timeStamp = this.time.timestamp,
+
+        homeTeam = Team(
+            name = this.teams.home.name,
+            image = this.teams.home.img
+        ),
+        awayTeam = Team(
+            name = this.teams.away.name,
+            image = this.teams.away.img
         )
-    }
+    )
+}
 
-    fun mapDbModelToDto(dbModel : GameDbModel) : GameDto {
-        return GameDto(
-            id = dbModel.id,
-            league = League(
-                country_flag = dbModel.league.country_flag,
-                country_name = dbModel.league.country_name,
-                name = dbModel.league.name
-            ),
-            teams = Teams(
-                away = Away(
-                    img = dbModel.awayTeam.image,
-                    name = dbModel.awayTeam.name
-                ),
-                home = Home(
-                    img = dbModel.homeTeam.image,
-                    name = dbModel.homeTeam.name
-                )
-            ),
-            venue_id = dbModel.venueId,
-            stage_id = dbModel.stageId,
-            stage_name = dbModel.stageName,
-            time = Time(
-                time = dbModel.time,
-                datetime = dbModel.dateTime,
-                timestamp = dbModel.timeStamp
-            ),
-            round_name = dbModel.roundName
+fun GameDbModel.toEntity(): Game {
+    val dto = this.toDto()
+    return dto.toEntity()
+}
+
+fun Game.toDbModel(): GameDbModel {
+    return GameDbModel(
+        id = this.id,
+        league = LeagueDbModel(
+            name = this.league.name,
+            country_name = this.league.country_name,
+            country_flag = this.league.country_flag
+        ),
+        venueId = this.venueId,
+        stageId = this.stageId,
+        stageName = this.stageName,
+        roundName = this.roundName,
+
+        time = this.time,
+        dateTime = this.dateTime,
+        timeStamp = this.timeStamp,
+
+        homeTeam = TeamDbModel(
+            name = this.homeTeam.name,
+            image = this.homeTeam.image
+        ),
+        awayTeam = TeamDbModel(
+            name = this.awayTeam.name,
+            image = this.awayTeam.image
         )
-    }
-
-    fun mapDtoToEntity(dto: GameDto) : Game {
-        return Game(
-            id = dto.id,
-            league = com.example.sportapp.domain.entities.League(
-                name = dto.league.name,
-                country_name = dto.league.country_name,
-                country_flag = dto.league.country_flag
-            ),
-            venueId = dto.venue_id,
-            stageId = dto.stage_id,
-            stageName = dto.stage_name,
-            roundName = dto.round_name,
-
-            time = dto.time.time,
-            dateTime = dto.time.datetime,
-            timeStamp = dto.time.timestamp,
-
-            homeTeam = Team(
-                name = dto.teams.home.name,
-                image = dto.teams.home.img
-            ),
-            awayTeam = Team(
-                name = dto.teams.away.name,
-                image = dto.teams.away.img
-            )
-        )
-    }
-
-    fun mapDbModelToEntity(dbModel: GameDbModel) : Game {
-        val mapper = GameMapper()
-        val dto = mapper.mapDbModelToDto(dbModel)
-        return mapper.mapDtoToEntity(dto)
-    }
-
-    fun mapEntityToDbModel(game : Game) : GameDbModel {
-        return GameDbModel(
-            id = game.id,
-            league = LeagueDbModel(
-                name = game.league.name,
-                country_name = game.league.country_name,
-                country_flag = game.league.country_flag
-            ),
-            venueId = game.venueId,
-            stageId = game.stageId,
-            stageName = game.stageName,
-            roundName = game.roundName,
-
-            time = game.time,
-            dateTime = game.dateTime,
-            timeStamp = game.timeStamp,
-
-            homeTeam = TeamDbModel(
-                name = game.homeTeam.name,
-                image = game.homeTeam.image
-            ),
-            awayTeam = TeamDbModel(
-                name = game.awayTeam.name,
-                image = game.awayTeam.image
-            )
-        )
-    }
+    )
 }
